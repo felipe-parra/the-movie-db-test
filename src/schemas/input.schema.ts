2b9b9114361a857c750bd49786ba9be7d9f371b7
Felipe Parra <fpr29.10@gmail.com>
2024-02-02 10:08:46 -0600

    Updated with validations

--- src/schemas/input.schema.ts
+++ src/schemas/input.schema.ts
@@ -1,3 +1,10 @@
 import { z } from "zod";
 
-export const input = z.object({});
+export const searchFormSchema = z.object({
+  searchQuery: z
+    .string()
+    .min(1, { message: "Must be at least 1 character" })
+    .max(10, { message: "Must be less than 10 characters" }),
+});
+
+export type SearchFormSchema = z.infer<typeof searchFormSchema>;
