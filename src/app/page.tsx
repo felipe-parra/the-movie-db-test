09a319e4bd1c7851a5208c66903de85bab1c0736
Felipe Parra <fpr29.10@gmail.com>
2024-02-02 11:23:00 -0600

    Added real use

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,28 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { searchMovie } from './actions'
+import { MovieList } from '@/components/movies/MovieList'
+import SearchInput from '@/components/search-input'
 import useStore from '@/store/useStore'
 import { useMovieStore } from '@/store/useMovieStore'
-import { QueryClient, useQuery } from '@tanstack/react-query'
-import { searchMovie } from './actions'
+import { useEffect } from 'react'
 
 export default function HomePage() {
-  const addCount = useMovieStore((state) => state.addCount)
-  const count = useStore(useMovieStore, (state) => state.count)
-  const [movies, setMovies] = useState([])
+  const query = useStore(useMovieStore, (state) => state.query)
   const { data, isLoading, isError } = useQuery({
-    queryKey: ["search-movies"],
-    queryFn: () => searchMovie("spiderman"),
-    staleTime: 5 * 1000
+    queryKey: ["search-movies", query],
+    queryFn: () => searchMovie(query || ""),
+    staleTime: 5 * 1000,
+
   })
 
-  if (isLoading) return <p>Loading...</p>
   return (
     <section>
       <article>
-        <h1>
-          Home
-        </h1>
+        <SearchInput />
       </article>
-      <button className='text-white text-2xl' onClick={addCount}>
-        {count}
-      </button>
-      <article>
-        {
-          data?.results.map((movie) => <p key={movie.id}>{movie?.title}</p>)
-        }
+      <article className='w-full'>
+        <MovieList movies={data?.results.slice(0, 3)} isError={isError} isLoading={isLoading} />
       </article>
     </section>
   )
